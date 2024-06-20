225779388579a20bf43439ff5430d0c268ee61f6
EnoshMcCarthy <enoshh12@gmail.com>
2024-06-20 10:58:57 +0300

    added logs

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import connectDB from "./database";
 import cors from "cors";
 import dotenv from "dotenv";
 import foodItemRouter from "./routes/foodItemRoute";
+import morgan from "morgan";
+import logger from "./logger/logger";
 
 dotenv.config();
 
@@ -11,6 +13,12 @@ const PORT = process.env.PORT || 4899;
 
 app.use(express.json());
 app.use(cors());
+app.use(
+  morgan("combined", {
+    stream: { write: (message) => logger.info(message.trim()) },
+  })
+);
+
 app.use("/api/food-item/", foodItemRouter);
 
 app.listen(PORT, async () => {
--- src/routes/foodItemRoute.ts
+++ src/routes/foodItemRoute.ts
@@ -1,16 +1,25 @@
 import { Request, Response, Router } from "express";
 import FoodItem, { IFoodItem } from "../model/foodItem";
+import logger from "../logger/logger";
 
 const foodItemRouter = Router();
 
 foodItemRouter.post("/new", async (req: Request, res: Response) => {
   const item: IFoodItem = req.body;
 
+  logger.info(
+    `[FoodItem-New] Request for new food item:${JSON.stringify(item)}`
+  );
+
   try {
     const foodItem = new FoodItem(item);
     await foodItem.save();
+    logger.info(`[FoodItem-New] Food item was saved successfully`);
+
     return res.status(200).json({ message: "success", item });
   } catch (error) {
+    logger.error(`[FoodItem-New] Error saving food item: ${error}`);
+
     res.status(500).json({ message: `Error creating food item: ${error}` });
   }
 });
@@ -20,21 +29,31 @@ foodItemRouter.get("/get/:name", async (req: Request, res: Response) => {
   try {
     const foodItem = await FoodItem.findOne({ name });
     if (!foodItem) {
+    logger.error(`[FoodItem-New] Food item by the name: ${name} doesnt exists`);
+
       return res.status(400).json({ message: "Food item not found" });
     }
+    logger.info(`[FoodItem-New] Food item by the name: ${name} exists`);
+
     return res.status(200).json(foodItem);
   } catch (error) {
+    logger.error(`[FoodItem-New] Error finding food item: ${error}`);
+
     return res
       .status(500)
       .json({ message: `Error finding food item: ${error}` });
   }
 });
 
-foodItemRouter.get("/all", async (req: Request, res: Response) => {
+foodItemRouter.get("/all", async (_: Request, res: Response) => {
   try {
     const foodItems = await FoodItem.find({});
+    logger.info(`[FoodItem-All] Supplying all food items`);
+
     return res.status(200).json(foodItems);
   } catch (error) {
+    logger.error(`[FoodItem-All] Error supplying all food items: ${error}`);
+
     return res
       .status(500)
       .json({ message: `Error retrieving food items: ${error}` });
